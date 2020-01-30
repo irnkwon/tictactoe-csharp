a3ec59008d55cabf1a17d6267c1693e215d142da
irnkwon <haeryun2004@gmail.com>
2020-01-30 13:01:01 -0500

    Added tic tac toe files

---
+++ HKwonAssignment2/HKwonAssignment2/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace HKwonAssignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn1.Location = new System.Drawing.Point(0, 0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(192, 196);
+            this.btn1.TabIndex = 0;
+            this.btn1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn2.Location = new System.Drawing.Point(223, 0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(156, 196);
+            this.btn2.TabIndex = 1;
+            this.btn2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn3.Location = new System.Drawing.Point(410, 0);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(192, 196);
+            this.btn3.TabIndex = 2;
+            this.btn3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn7.Location = new System.Drawing.Point(0, 413);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(192, 187);
+            this.btn7.TabIndex = 3;
+            this.btn7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn9.Location = new System.Drawing.Point(410, 413);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(192, 187);
+            this.btn9.TabIndex = 4;
+            this.btn9.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn4.Location = new System.Drawing.Point(0, 224);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(192, 162);
+            this.btn4.TabIndex = 5;
+            this.btn4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn6.Location = new System.Drawing.Point(410, 224);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(192, 162);
+            this.btn6.TabIndex = 6;
+            this.btn6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn5.Location = new System.Drawing.Point(223, 224);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(156, 162);
+            this.btn5.TabIndex = 7;
+            this.btn5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn8.Location = new System.Drawing.Point(223, 413);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(156, 187);
+            this.btn8.TabIndex = 8;
+            this.btn8.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn8;
+    }
+}
+
---
+++ HKwonAssignment2/HKwonAssignment2/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//
+// Assignment 2: Tic-Tac-Toe
+//
+// HKwonAssignment2
+//  By: Haeryun Kwon (Irene)
+//  Date Created: 10.08.2017
+//
+
+namespace HKwonAssignment2
+{
+    public partial class Form1 : Form
+    {
+        // Initialize variables
+        string oWinMsg = "O Win!";
+        string xWinMsg = "X Win!";
+        string drawMsg = "Draw!";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btn1.Click += new System.EventHandler(ClickedButton);
+            btn2.Click += new System.EventHandler(ClickedButton);
+            btn3.Click += new System.EventHandler(ClickedButton);
+            btn4.Click += new System.EventHandler(ClickedButton);
+            btn5.Click += new System.EventHandler(ClickedButton);
+            btn6.Click += new System.EventHandler(ClickedButton);
+            btn7.Click += new System.EventHandler(ClickedButton);
+            btn8.Click += new System.EventHandler(ClickedButton);
+            btn9.Click += new System.EventHandler(ClickedButton);
+        }
+
+        /****************************************************************************
+                                        Form Buttons
+         ****************************************************************************/
+        public void ClickedButton(object sender, EventArgs e)
+        {
+            // Initialize variables
+            string btnAllText = btn1.Text + btn2.Text + btn3.Text + btn4.Text + btn5.Text + btn6.Text + btn7.Text + btn8.Text + btn9.Text;
+
+            // If the concatenated texts or all buttons are empty, it starts with 'x'.
+            // Also, need to check whether a clicked button is empty or not.
+            if (btnAllText == "" && (sender as Button).Text == "") // 1st turn
+            {
+                (sender as Button).Text = "x";
+                (sender as Button).Image = Properties.Resources.X;
+            }
+
+            if (btnAllText == "x" && (sender as Button).Text == "") // 2nd turn
+            {
+                (sender as Button).Text = "o";
+                (sender as Button).Image = Properties.Resources.O;
+            }
+
+            if (btnAllText.Length == 2 && (sender as Button).Text == "") // 3rd turn
+            {
+                (sender as Button).Text = "x";
+                (sender as Button).Image = Properties.Resources.X;
+            }
+
+            if (btnAllText.Length == 3 && (sender as Button).Text == "") // 4th turn
+            {
+                (sender as Button).Text = "o";
+                (sender as Button).Image = Properties.Resources.O;
+            }
+
+            if (btnAllText.Length == 4 && (sender as Button).Text == "") // 5th turn
+            {
+                (sender as Button).Text = "x";
+                (sender as Button).Image = Properties.Resources.X;
+            }
+
+            if (btnAllText.Length == 5 && (sender as Button).Text == "") // 6th turn
+            {
+                (sender as Button).Text = "o";
+                (sender as Button).Image = Properties.Resources.O;
+            }
+
+            if (btnAllText.Length == 6 && (sender as Button).Text == "") // 7th turn
+            {
+                (sender as Button).Text = "x";
+                (sender as Button).Image = Properties.Resources.X;
+            }
+
+            if (btnAllText.Length == 7 && (sender as Button).Text == "") // 8th turn
+            {
+                (sender as Button).Text = "o";
+                (sender as Button).Image = Properties.Resources.O;
+            }
+
+            if (btnAllText.Length == 8 && (sender as Button).Text == "") // 9th turn
+            {
+                (sender as Button).Text = "x";
+                (sender as Button).Image = Properties.Resources.X;
+            }
+
+            // After every player-turn, it should check if there is a winner.
+            CheckWinner();
+        }
+
+        /****************************************************************************
+                                      Check for a Win
+         ****************************************************************************/
+
+        public void CheckWinner()
+        {
+            if (btn1.Text + btn2.Text + btn3.Text == "ooo" || btn4.Text + btn5.Text + btn6.Text == "ooo" || btn7.Text + btn8.Text + btn9.Text == "ooo" ||
+                btn1.Text + btn4.Text + btn7.Text == "ooo" || btn2.Text + btn5.Text + btn8.Text == "ooo" || btn3.Text + btn6.Text + btn9.Text == "ooo" ||
+                btn1.Text + btn5.Text + btn9.Text == "ooo" || btn3.Text + btn5.Text + btn7.Text == "ooo")
+            {
+                MessageBox.Show(oWinMsg);
+                AskNewGame();
+            }
+
+            else if (btn1.Text + btn2.Text + btn3.Text == "xxx" || btn4.Text + btn5.Text + btn6.Text == "xxx" || btn7.Text + btn8.Text + btn9.Text == "xxx" ||
+                btn1.Text + btn4.Text + btn7.Text == "xxx" || btn2.Text + btn5.Text + btn8.Text == "xxx" || btn3.Text + btn6.Text + btn9.Text == "xxx" ||
+                btn1.Text + btn5.Text + btn9.Text == "xxx" || btn3.Text + btn5.Text + btn7.Text == "xxx")
+            {
+                MessageBox.Show(xWinMsg);
+                AskNewGame();
+            }
+
+            else
+            {
+                CheckDraw();
+            }
+        }
+
+        /****************************************************************************
+                                    Check for a Draw
+         ****************************************************************************/
+        private void CheckDraw()
+        {
+            if (btn1.Text.Length > 0 & btn2.Text.Length > 0 & btn3.Text.Length > 0 &
+                btn4.Text.Length > 0 & btn5.Text.Length > 0 & btn6.Text.Length > 0 &
+                btn7.Text.Length > 0 & btn8.Text.Length > 0 & btn9.Text.Length > 0)
+            {
+                MessageBox.Show(drawMsg);
+                AskNewGame();
+            }
+        }
+
+        /****************************************************************************
+                                      Ask New Game
+         ****************************************************************************/
+
+        private void AskNewGame()
+        {
+            // Show a message box asking whether a user want to continue the game or not.
+            DialogResult newGame = MessageBox.Show("New Game?", "Tic Tac Toe", MessageBoxButtons.YesNo);
+
+            // If 'Yes'
+            if(newGame == DialogResult.Yes)
+            {
+                btn1.Text = "";
+                btn2.Text = "";
+                btn3.Text = "";
+                btn4.Text = "";
+                btn5.Text = "";
+                btn6.Text = "";
+                btn7.Text = "";
+                btn8.Text = "";
+                btn9.Text = "";
+
+                btn1.Image = null;
+                btn2.Image = null;
+                btn3.Image = null;
+                btn4.Image = null;
+                btn5.Image = null;
+                btn6.Image = null;
+                btn7.Image = null;
+                btn8.Image = null;
+                btn9.Image = null;
+            }
+
+            else // If 'No'
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
